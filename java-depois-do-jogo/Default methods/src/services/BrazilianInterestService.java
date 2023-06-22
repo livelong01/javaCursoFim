403dae6b73dc60c625010ed9e939da47eab5e47c
jonathan alonso <jonathanalonsom@outlook.com>
2023-06-22 14:40:32 +0100

    Default methods-  com Interface simples

---
+++ java-depois-do-jogo/Default methods/src/services/BrazilianInterestService.java
@@ -0,0 +1,26 @@
+package services;
+
+import java.security.InvalidParameterException;
+
+public class BrazilianInterestService  implements InterestService{
+
+	private Double interestRate;
+
+	public BrazilianInterestService(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+	@Override
+	public double payment( double amount, int months) {
+		if (months <1) {
+			throw new InvalidParameterException("Months must be greater than zero");
+		}
+		return amount * Math.pow(1.0 + interestRate/ 100.0, months);
+	}
+	
+}
