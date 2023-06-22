23d07d822f254be92425c8718c8b36c8bd39b938
jonathan alonso <jonathanalonsom@outlook.com>
2023-06-22 13:35:38 +0100

    Ordenar funcionarios pelo nome

---
+++ java-depois-do-jogo/ComparableTo/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import Entities.Employee;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Employee> list = new ArrayList<>();
+		String path = "/home/jonathan/Documentos/in";
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
+			}
+			
+			/*
+			 * Collections.sort é um funcao que ordena
+			 * listas por ordem alfabetica.
+			 */
+			Collections.sort(list);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() +", " + emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
---
+++ java-depois-do-jogo/ProblemaDoDiamante_ComboDevice/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ConcretePrinter p = new ConcretePrinter("1080");
+		p.processDoc("My Letter");
+		p.print("My Letter");
+		
+		System.out.println();
+		ConcreteScanner s = new ConcreteScanner ("2003");
+		s.processDoc("My Email");
+		System.out.println("Scan result: " + s.scan());
+
+		System.out.println();
+		ComboDevice c = new ComboDevice("2081");
+		c.processDoc("My dissertarion");
+		c.print("My dissertarion");
+		System.out.println("Scan Result: " + c.scan());
+	}
+
+}
