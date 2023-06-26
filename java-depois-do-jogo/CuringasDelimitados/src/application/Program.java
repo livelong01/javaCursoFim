1746168503fcf9015546c23cadcf67ba59664c54
jonathan alonso <jonathanalonsom@outlook.com>
2023-06-26 13:50:13 +0100

    COringa delimitado: Metodo usando ? extends

--- java-depois-do-jogo/CuringasDelimitados/src/application/Program.java
+++ java-depois-do-jogo/CuringasDelimitados/src/application/Program.java
@@ -16,11 +16,24 @@ public class Program {
 		myShape.add(new Rectangle (3.0, 2.0));
 		myShape.add(new Circle(2.0));
 		
-		System.out.println("Total AREA: " + totalArea(myShape));
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(2.0));
+		myCircle.add(new Circle(3.0));
+		
+		System.out.println("Total AREA: " + totalArea(myCircle));
 
 	}
-
-	public static double totalArea(List<Shape> list) {
+/*
+ * Nessa funcao, sempre da erro se usar Shape ou ?, 
+ * o primeiro caso da errado, pq tem q ser sempre uma super classe
+ * e o circle é uma subclasse, dai nao funciona.
+ * Na ? n funciona pq n pode fazer soma com tipo coringa.
+ * A solucao é: "List<? extends Shape", pq ai ele reconhece
+ * que a lista é extensao de Shape ou qualquer tipo de lista
+ * com subtipo de Shape, dai funciona perfeitamente.
+ */
+	
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for (Shape s : list ) {
 			sum += s.area();
