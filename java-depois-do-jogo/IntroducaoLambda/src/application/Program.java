e6d8712c655addcbd8a50882b93c6f7f00b3ffef
jonathan alonso <jonathanalonsom@outlook.com>
2023-07-10 10:48:24 +0100

    Versao do COmparator dentro do programa principal

--- java-depois-do-jogo/IntroducaoLambda/src/application/Program.java
+++ java-depois-do-jogo/IntroducaoLambda/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,13 +16,25 @@ public class Program {
 		list.add(new Product ("Notebook", 1200.00));
 		list.add(new Product ("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().compareToIgnoreCase(p2.getName());
+			}
+			
+		};
 		
 		/*
-		 * Deve-se entrar no "sort" com um comparator, ou seja, 
-		 * uma expressao lambda e ela fara todo o trabalho de
-		 * comparar e ordenar a lista.
+		 * Nesse caso, ele criou um compareTO ANONIMO DENTRO DA CLASSE principal
+		 * NAO PRECISA mais de uma classe separada (myCOmparator) para fazer
+		 * esse serviço. Essa faz direto. Entretanto, essa declaracao
+		 * dentro da classe e do tamanho que é, nao fica bonito e nem é
+		 * recomendado.
 		 */
+		
+		list.sort(comp);
+		
 		
 		for (Product p : list) {
 			System.out.println(p);
