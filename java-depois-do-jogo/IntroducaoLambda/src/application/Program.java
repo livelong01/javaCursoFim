fe8f71034995f78f67a2797d675e0ec961d6522c
jonathan alonso <jonathanalonsom@outlook.com>
2023-07-10 10:57:03 +0100

    Versao mais enxuta do Arrow Function

--- java-depois-do-jogo/IntroducaoLambda/src/application/Program.java
+++ java-depois-do-jogo/IntroducaoLambda/src/application/Program.java
@@ -16,16 +16,8 @@ public class Program {
 		list.add(new Product ("Notebook", 1200.00));
 		list.add(new Product ("Tablet", 450.00));
 		
-		Comparator<Product> comp = (p1, p2) ->{
-			return p1.getName().compareToIgnoreCase(p2.getName());
-		};
-		
-		/*
-		 * Nesse caso, o comparator foi feito atraves de uma ARROW FUNCTION
-		 * por causa da setinha "->". Ela vc apenas precisa determinar
-		 * os parametros (p1 e p2), fazer a seta, e entre chaves
-		 * descrever a funcao "1.getName().compareToIgnoreCase..."
-		 */
+		Comparator<Product> comp = (p1, p2) ->p1.getName().compareToIgnoreCase(p2.getName());
+
 		
 		list.sort(comp);
 		
