7af64cd5fcbf8b04d2e0b99c0509c50a47610c50
jonathan alonso <jonathanalonsom@outlook.com>
2023-07-13 09:59:26 +0100

    Predicate: vs1 - Implementacao da Interface

---
+++ java-depois-do-jogo/predicate/src/entities/Product.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double Price;
+	
+	
+	
+	public Product(String name, Double price) {
+		this.name = name;
+		Price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return Price;
+	}
+	public void setPrice(Double price) {
+		Price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", Price=" + Price + "]";
+	}
+}
