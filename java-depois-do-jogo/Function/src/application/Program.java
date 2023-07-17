79585fbbaf0a6fff56a376c9053776abbaa2bde7
jonathan alonso <jonathanalonsom@outlook.com>
2023-07-17 10:20:01 +0100

    Function: vs2 Reference method com metodo estatico

--- java-depois-do-jogo/Function/src/application/Program.java
+++ java-depois-do-jogo/Function/src/application/Program.java
@@ -47,7 +47,7 @@ Locale.setDefault(Locale.US);
 		 * 
 		 */
 		
-		List <String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		List <String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 
