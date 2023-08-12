0c25520f0f4f57e3569bafa4e3d88d5a8fccb9da
jonathan alonso <jonathanalonsom@outlook.com>
2023-08-12 13:25:23 +0100

    JDB4 - Atualizando dados

---
+++ java-depois-do-jogo/jdbc4/src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.awt.image.DataBufferByte;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Atualizar o salario de um vendedor na base de dados.
+		 */
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			/*
+			 * UPDATE  =  ATUALIZAR OS seller 
+			 * Set, modificar a coluna BaseSalary e o "?" é o valor adc.
+			 * NÃO ESQUECER DO WHERE!!!
+			 * Ele é a condicional, sem ele todos os salarios serao modificados.
+			 * UM DESESTRE!
+			 * A condicao no caso, é : ONDE O DEPARTMENTID = "?" valor que darei 
+			 * no futuro.
+			 */
+			
+			st = conn.prepareStatement(
+					 "UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+					
+			st.setDouble(1, 200.00);
+			st.setInt(2, 2);
+			
+			int rownsAffected = st.executeUpdate();
+			
+			System.out.println("Done ROws Affected: " + rownsAffected);
+			
+		}
+		catch ( SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();		}
+	}
+}
