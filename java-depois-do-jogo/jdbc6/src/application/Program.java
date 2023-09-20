d39c242af85eecd943bd799bf7436a5af20305b1
jonathan alonso <jonathanalonsom@outlook.com>
2023-09-20 16:50:52 +0100

    Demo - Versao inicial com Fake error

---
+++ java-depois-do-jogo/jdbc6/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1" );
+			
+			int x = 1;
+			if (x<2) {
+				throw new SQLException("Fake error");
+			}
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2" );
+			
+			System.out.println("Rows1 = " + rows1);
+			System.out.println("Rows2 = " + rows2);
+		}
+		catch ( SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();		}
+	}
+}
